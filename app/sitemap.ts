42c952100562685a9b36959183d16fb792b7f2e7
unknown <m.aggoubi@mayagroup.ma>
2025-10-15 23:57:21 +0100

    build local

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,10 @@
 import { MetadataRoute } from 'next';
 import siteConfig from '@/lib/config';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = siteConfig.url;
-
   return [
     {
       url: baseUrl,
